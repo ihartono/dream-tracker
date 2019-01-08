9a37c929daf4f33241d32b390101b5b8512dc6a8
ihartono <iwan.hartono@tokopedia.com>
2019-01-08 10:51:39 +0700

    working on response json

--- DreamTracker/ListViewController.swift
+++ DreamTracker/ListViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+
+struct Dream : Decodable {
+    let description: String
+    let id : Int
+    let title: String
+}
+
 
 class ListViewController: UITableViewController {
 
@@ -14,7 +22,56 @@ class ListViewController: UITableViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("aaa")
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        
+        Alamofire.request(URL(string: "http://93.188.167.250:8080/me/dreams")!,
+                          method: .get,
+                          encoding: JSONEncoding.default,
+                          headers: [
+                            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZlckBmZXIuY29tIiwiaWQiOiIxIiwibmFtZSI6IkZlcmljbyJ9.XvvHvQyP8YuukKtyXsohpe1-H1BY-pWb94kbtnIL5PQ"])
+            .responseJSON { (response) in
+                switch response.result {
+                case .success(let JSON):
+                    let res = JSON as! NSDictionary
+                    let data = res.object(forKey: "data")!
+                    let success = res.object(forKey: "success") as! Int
+                    
+                    if success == 1 {
+                        let dataToArray = (data as! NSArray) as Array
+                        print(dataToArray)
+                        print("ahahahhahahahhahahhahhaha")
+                        
+                        dataToArray.forEach { eachDream in
+                            let description = eachDream["description"] as? String ?? "empty description"
+                            let id = eachDream["id"] as! Int
+                            let image_uri = eachDream["image_uri"] as? String ?? "emptyimageuri"
+                            let title = eachDream["title"] as? String ?? "empty title"
+                            let todo = res.object(forKey: "todo") ?? "empty object"
+                            let user_id = eachDream["user_id"] as! Int
+                            
+                            print(description)
+                            print(id)
+                            print(image_uri)
+                            print(title)
+                            print(todo)
+                            print(user_id)
+                        }
+                    }
+                    // TODO add condition when success is 0
+                case .failure(_ ):
+                    print("request failed")
+                }
+                
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        
+        cell.textLabel?.text = "ahaha"
+        
+        return cell
     }
     
     @IBAction func btnLogout(_ sender: Any) {
